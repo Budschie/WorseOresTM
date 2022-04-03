2cb5d1eed04925275162e29c5cbef04aaa064ac1
Budschie <jul.kp06@gmail.com>
2022-04-03 21:39:19 +0200

    Tweaked numbers a bit to produce better results on world gen

--- src/main/java/de/budschie/worseores/world_gen/FeatureRegistry.java
+++ src/main/java/de/budschie/worseores/world_gen/FeatureRegistry.java
@@ -38,61 +38,61 @@ public class FeatureRegistry
 	public static void addFeatures()
 	{
 		COPPER_FEATURE = register(new ResourceLocation(References.MODID, "copper_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.COPPER_ORE.get().defaultBlockState(), 12));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.COPPER_ORE.get().defaultBlockState(), 8));
 		
 		ALOTOSTUFFIUM_FEATURE = register(new ResourceLocation(References.MODID, "alotostuffium_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.ALOTOSTUFFIUM_ORE.get().defaultBlockState(), 7));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.ALOTOSTUFFIUM_ORE.get().defaultBlockState(), 7));
 		
 		AMADEUM_FEATURE = register(new ResourceLocation(References.MODID, "amadeum_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.AMADEUM_ORE.get().defaultBlockState(), 5));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.AMADEUM_ORE.get().defaultBlockState(), 6));
 		
 		APPETITE_FEATURE = register(new ResourceLocation(References.MODID, "appetite_ore_feature"), 
 				Feature.ORE, new OreConfiguration(new BlockMatchTest(Blocks.SAND), BlockRegistry.APPETITE_ORE.get().defaultBlockState(), 5));
 		
 		ENDERITE_FEATURE = register(new ResourceLocation(References.MODID, "enderite_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.ENDERITE_ORE.get().defaultBlockState(), 4));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.ENDERITE_ORE.get().defaultBlockState(), 4));
 		
 		EXPLODEMITE_FEATURE = register(new ResourceLocation(References.MODID, "explodemite_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.EXPLODEMITE_ORE.get().defaultBlockState(), 7));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.EXPLODEMITE_ORE.get().defaultBlockState(), 7));
 		
 		IDLIKEABITE_FEATURE = register(new ResourceLocation(References.MODID, "idlikeabite_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.IDLIKEABITE_ORE.get().defaultBlockState(), 10));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.IDLIKEABITE_ORE.get().defaultBlockState(), 12));
 		
 		SMITEANDLITE_FEATURE = register(new ResourceLocation(References.MODID, "smiteandlite_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.SMITEANDLITE_ORE.get().defaultBlockState(), 5));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.SMITEANDLITE_ORE.get().defaultBlockState(), 5));
 		
 		NOFITE_FEATURE = register(new ResourceLocation(References.MODID, "nofite_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.NOFITE_ORE.get().defaultBlockState(), 5));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.NOFITE_ORE.get().defaultBlockState(), 5));
 		
 		KILLIUM_FEATURE = register(new ResourceLocation(References.MODID, "killium_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.KILLIUM_ORE.get().defaultBlockState(), 5));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.KILLIUM_ORE.get().defaultBlockState(), 5));
 		
 		RANDOMIUM_FEATURE = register(new ResourceLocation(References.MODID, "randomium_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.RANDOMIUM_ORE.get().defaultBlockState(), 3));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.RANDOMIUM_ORE.get().defaultBlockState(), 3));
 		
 		MEMENITE_ORE = register(new ResourceLocation(References.MODID, "memenite_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.MEMENITE_ORE.get().defaultBlockState(), 5));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.MEMENITE_ORE.get().defaultBlockState(), 5));
 		
 		LITEINNITE_ORE = register(new ResourceLocation(References.MODID, "liteinnite_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.LITEINNITE_ORE.get().defaultBlockState(), 10));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.LITEINNITE_ORE.get().defaultBlockState(), 10));
 		
 		BESTFORGE_ORE = register(new ResourceLocation(References.MODID, "bestforge_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.BESTFORGE_ORE.get().defaultBlockState(), 5));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.BESTFORGE_ORE.get().defaultBlockState(), 6));
 	
 		DOGECOIN_ORE = register(new ResourceLocation(References.MODID, "dogecoin_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.DOGECOIN_ORE.get().defaultBlockState(), 5));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.DOGECOIN_ORE.get().defaultBlockState(), 4));
 	
 		MOBIUM_ORE = register(new ResourceLocation(References.MODID, "mobium_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.MOBIUM_ORE.get().defaultBlockState(), 10));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.MOBIUM_ORE.get().defaultBlockState(), 11));
 		
 		DREAMIUM_ORE = register(new ResourceLocation(References.MODID, "dreamium_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.DREAMIUM_ORE.get().defaultBlockState(), 5));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.DREAMIUM_ORE.get().defaultBlockState(), 7));
 		
 		DIAMOND_ORE = register(new ResourceLocation(References.MODID, "diamond_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.DIAMOND_ORE.get().defaultBlockState(), 8));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.DIAMOND_ORE.get().defaultBlockState(), 8));
 		
 		TRIPLITE_ORE = register(new ResourceLocation(References.MODID, "triplite_ore_feature"), 
-				Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.TRIPLITE_ORE.get().defaultBlockState(), 6));
+				Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, BlockRegistry.TRIPLITE_ORE.get().defaultBlockState(), 9));
 	}
 	
 	private static <FC extends FeatureConfiguration, F extends Feature<FC>> Holder<ConfiguredFeature<?, ?>> register(ResourceLocation rl, Feature<FC> feature, FC featureConfig)
--- src/main/java/de/budschie/worseores/world_gen/PlacedFeatureRegistry.java
+++ src/main/java/de/budschie/worseores/world_gen/PlacedFeatureRegistry.java
@@ -43,25 +43,25 @@ public class PlacedFeatureRegistry
 	
 	public static void addPlacedFeatures()
 	{
-		COPPER_FEATURE = registerRange(new ResourceLocation(References.MODID, "copper_ore"), FeatureRegistry.COPPER_FEATURE, 0, 60, 8);
-		ALOTOSTUFFIUM_FEATURE = registerRange(new ResourceLocation(References.MODID, "alotofstuffium_ore"), FeatureRegistry.ALOTOSTUFFIUM_FEATURE, -48, 60, 20);
-		AMADEUM_FEATURE = registerRange(new ResourceLocation(References.MODID, "amadeum_ore"), FeatureRegistry.AMADEUM_FEATURE, -40, 60, 20);
-		APPETITE_FEATURE = registerRange(new ResourceLocation(References.MODID, "appetite_ore"), FeatureRegistry.APPETITE_FEATURE, 0, 60, 12);
-		ENDERITE_FEATURE = registerRange(new ResourceLocation(References.MODID, "enderite_ore"), FeatureRegistry.ENDERITE_FEATURE, -48, 50, 20);
-		EXPLODEMITE_FEATURE = registerRange(new ResourceLocation(References.MODID, "explodemite_ore"), FeatureRegistry.EXPLODEMITE_FEATURE, 0, 60, 12);
-		IDLIKEABITE_FEATURE = registerRange(new ResourceLocation(References.MODID, "idlikeabite_ore"), FeatureRegistry.IDLIKEABITE_FEATURE, 0, 60, 12);
+		COPPER_FEATURE = registerRange(new ResourceLocation(References.MODID, "copper_ore"), FeatureRegistry.COPPER_FEATURE, 0, 60, 9);
+		ALOTOSTUFFIUM_FEATURE = registerRange(new ResourceLocation(References.MODID, "alotofstuffium_ore"), FeatureRegistry.ALOTOSTUFFIUM_FEATURE, 8, 60, 6);
+		AMADEUM_FEATURE = registerRange(new ResourceLocation(References.MODID, "amadeum_ore"), FeatureRegistry.AMADEUM_FEATURE, 0, 60, 7);
+		APPETITE_FEATURE = registerRange(new ResourceLocation(References.MODID, "appetite_ore"), FeatureRegistry.APPETITE_FEATURE, 40, 80, 12);
+		ENDERITE_FEATURE = registerRange(new ResourceLocation(References.MODID, "enderite_ore"), FeatureRegistry.ENDERITE_FEATURE, 0, 50, 8);
+		EXPLODEMITE_FEATURE = registerRange(new ResourceLocation(References.MODID, "explodemite_ore"), FeatureRegistry.EXPLODEMITE_FEATURE, 0, 60, 7);
+		IDLIKEABITE_FEATURE = registerRange(new ResourceLocation(References.MODID, "idlikeabite_ore"), FeatureRegistry.IDLIKEABITE_FEATURE, 0, 60, 9);
 		SMITEANDLITE_FEATURE = registerRange(new ResourceLocation(References.MODID, "smiteandlite_ore"), FeatureRegistry.SMITEANDLITE_FEATURE, 30, 60, 8);
-		NOFITE_FEATURE = registerRange(new ResourceLocation(References.MODID, "nofite_ore"), FeatureRegistry.NOFITE_FEATURE, -12, 60, 12);
-		KILLIUM_FEATURE = registerRange(new ResourceLocation(References.MODID, "killium_ore"), FeatureRegistry.KILLIUM_FEATURE, -48, 0, 12);
-		RANDOMIUM_FEATURE = registerRange(new ResourceLocation(References.MODID, "randomium_ore"), FeatureRegistry.RANDOMIUM_FEATURE, -60, 60, 16);
-		MEMENITE_ORE = registerRange(new ResourceLocation(References.MODID, "memenite_ore"), FeatureRegistry.MEMENITE_ORE, 0, 60, 12);
-		LITEINNITE_ORE = registerRange(new ResourceLocation(References.MODID, "liteinnite_ore"), FeatureRegistry.LITEINNITE_ORE, -60, 60, 16);
+		NOFITE_FEATURE = registerRange(new ResourceLocation(References.MODID, "nofite_ore"), FeatureRegistry.NOFITE_FEATURE, 0, 60, 7);
+		KILLIUM_FEATURE = registerRange(new ResourceLocation(References.MODID, "killium_ore"), FeatureRegistry.KILLIUM_FEATURE, 0, 60, 8);
+		RANDOMIUM_FEATURE = registerRange(new ResourceLocation(References.MODID, "randomium_ore"), FeatureRegistry.RANDOMIUM_FEATURE, 0, 60, 10);
+		MEMENITE_ORE = registerRange(new ResourceLocation(References.MODID, "memenite_ore"), FeatureRegistry.MEMENITE_ORE, 0, 60, 6);
+		LITEINNITE_ORE = registerRange(new ResourceLocation(References.MODID, "liteinnite_ore"), FeatureRegistry.LITEINNITE_ORE, 0, 60, 8);
 		BESTFORGE_ORE = registerRange(new ResourceLocation(References.MODID, "bestforge_ore"), FeatureRegistry.BESTFORGE_ORE, -60, 60, 12);
-		DOGECOIN_ORE = registerRange(new ResourceLocation(References.MODID, "dogecoin_ore"), FeatureRegistry.DOGECOIN_ORE, -48, 0, 12);
-		MOBIUM_ORE = registerRange(new ResourceLocation(References.MODID, "mobium_ore"), FeatureRegistry.MOBIUM_ORE, -60, 0, 16);
-		DREAMIUM_ORE = registerRange(new ResourceLocation(References.MODID, "dreamium_ore"), FeatureRegistry.DREAMIUM_ORE, 0, 60, 12);
-		DIAMOND_ORE = registerRange(new ResourceLocation(References.MODID, "diamond_ore"), FeatureRegistry.DIAMOND_ORE, 5, -20, 12);
-		TRIPLITE_ORE = registerRange(new ResourceLocation(References.MODID, "triplite_ore"), FeatureRegistry.TRIPLITE_ORE, -60, 60, 12);
+		DOGECOIN_ORE = registerRange(new ResourceLocation(References.MODID, "dogecoin_ore"), FeatureRegistry.DOGECOIN_ORE, 0, 48, 8);
+		MOBIUM_ORE = registerRange(new ResourceLocation(References.MODID, "mobium_ore"), FeatureRegistry.MOBIUM_ORE, 0, 20, 4);
+		DREAMIUM_ORE = registerRange(new ResourceLocation(References.MODID, "dreamium_ore"), FeatureRegistry.DREAMIUM_ORE, 0, 60, 8);
+		DIAMOND_ORE = registerRange(new ResourceLocation(References.MODID, "diamond_ore"), FeatureRegistry.DIAMOND_ORE, 0, 60, 12);
+		TRIPLITE_ORE = registerRange(new ResourceLocation(References.MODID, "triplite_ore"), FeatureRegistry.TRIPLITE_ORE, 0, 60, 8);
 	}
 	
 	@SubscribeEvent
@@ -93,7 +93,7 @@ public class PlacedFeatureRegistry
 	
 	private static Holder<PlacedFeature> registerRange(ResourceLocation rl, Holder<ConfiguredFeature<?, ?>> featureBase, int from, int to, int count)
 	{
-		return registerPlacedFeature(rl, new PlacedFeature(featureBase, Arrays.asList(CountPlacement.of(count), HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(from), VerticalAnchor.aboveBottom(to)))));
+		return registerPlacedFeature(rl, new PlacedFeature(featureBase, Arrays.asList(CountPlacement.of(count), HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(from + 64), VerticalAnchor.aboveBottom(to + 64)))));
 	}
 	
 	private static Holder<PlacedFeature> registerPlacedFeature(ResourceLocation rl, PlacedFeature feature)
